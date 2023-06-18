0fc58bbf50d0b5ddea648da4a4a9b184b1c6753b
+ <+>
2023-06-18 17:00:49 +0300

    third assignment complete

--- speed-detector.js
+++ speed-detector.js
@@ -1,55 +1,34 @@
-function speedDetectorr (speed) {
-    if (speed <= 70) {
-        return "Ok";
-    }
-    else if (speed > 70 && speed <= 75)
-        return "Points:2"
-}
+//This function is used to detect by how many km a car travels above the speed limit.
 
 function speedDetector (speed) {
-    let speedLimit = speed - 70
+    let speedLimit = speed - 70 // This calculates by how many km a car went above the speed limit of 70
     if (speedLimit <= 0) {
         return "Ok";
-    }
-    else if (speedLimit <= 5) {
-        return "Points:1"
-    }
-    else if (speedLimit > 5 && speedLimit <= 10) {
-        return "Points:2"
-    }
-    else if (speedLimit > 10 && speedLimit <= 15) {
-        return "Points:3"
-    }
-    else if (speedLimit > 15 && speedLimit <= 20) {
-        return "Points:4"
-    }
-    else if (speedLimit > 20 && speedLimit <= 25) {
-        return "Points:5"
-    }
-    else if (speedLimit > 25 && speedLimit <= 30) {
-        return "Points:6"
-    } 
-    else if (speedLimit > 30 && speedLimit <= 35) {
-        return "Points:7"
-    }
-    else if (speedLimit > 35 && speedLimit <= 40) {
-        return "Points:8"
-    }
-    else if (speedLimit > 40 && speedLimit <= 45) {
-        return "Points:9"
-    }
-    else if (speedLimit > 45 && speedLimit <= 50) {
-        return "Points:10"
-    }
-    else if (speedLimit > 50 && speedLimit <= 55) {
-        return "Points:11"
-    }
-    else if (speedLimit >55 && speedLimit <= 60) {
-        return "Points 12"
-    }
-    else if (speedLimit >60) {
-        return "Licence suspended"
+    }else if (speedLimit > 0 && speedLimit <= 5) { //This shows the demerit points a driver is awarded depending
+        return "Points:1";                         // on how many km they went above the speed limit
+    }else if (speedLimit > 5 && speedLimit <= 10) {
+        return "Points:2";
+    }else if (speedLimit > 10 && speedLimit <= 15) {
+        return "Points:3";
+    }else if (speedLimit > 15 && speedLimit <= 20) {
+        return "Points:4";
+    }else if (speedLimit > 20 && speedLimit <= 25) {
+        return "Points:5";
+    }else if (speedLimit > 25 && speedLimit <= 30) {
+        return "Points:6";
+    }else if (speedLimit > 30 && speedLimit <= 35) {
+        return "Points:7";
+    }else if (speedLimit > 35 && speedLimit <= 40) {
+        return "Points:8";
+    }else if (speedLimit > 40 && speedLimit <= 45) {
+        return "Points:9";
+    }else if (speedLimit > 45 && speedLimit <= 50) {
+        return "Points:10";
+    }else if (speedLimit > 50 && speedLimit <= 55) {
+        return "Points:11";
+    }else if (speedLimit >55 && speedLimit <= 60) {
+        return "Points 12";
+    }else if (speedLimit >60) { // any speed limit above 60, the licence is suspended.
+        return "Licence suspended";
     }
 }
-
-console.log(speedDetector(10))
--- student-grade-generator.js
+++ student-grade-generator.js
@@ -1,20 +1,18 @@
+// This function outputs various student grades between A and E according to the marks achieved between 0 and 100. 
+// If a user inputs marks ouside of this range whether < 0 or > 100, the functions outputs "Invalid marks."
 function studentGradeGenerator(grade) {
-    
-    if (grade > 79 && grade <= 100) {
-        return "A";
-    } 
-    else if (grade > 60 && grade <=79) {
-        return "B";
+    if (grade >= 0 && grade <=100) {
+        if (grade > 79 && grade <= 100) {
+            return "A";
+        }else if (grade > 60 && grade <=79) {
+            return "B";
+        }else if (grade > 49 && grade <= 59) {
+            return "C";
+        }else if (grade > 40 && grade <=49) {
+            return "D";
+        }else if (grade >= 0 && grade <= 40) {
+            return "E";
     }
-    else if (grade > 49 && grade <= 59) {
-        return "C";
-    }
-    else if (grade > 40 && grade <=49) {
-        return "D";
-    }
-    else if (grade < 40) {
-        return "E";
-    }
+    } else
+    return "Invalid Marks";
 }
- 
-console.log(studentGradeGenerator(90))
