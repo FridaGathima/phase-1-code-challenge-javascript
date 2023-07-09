449dc5ec67bcc34f738e20b1aaa19bcf03e3bb96
+ <+>
2023-07-09 12:21:38 +0300

    student-grade

--- student-grade-generator.js
+++ student-grade-generator.js
@@ -6,7 +6,7 @@ function studentGradeGenerator(marks) {
             return "A";
         }else if (marks > 60 && marks <=79) {
             return "B";
-        }else if (marks > 49 && marks <= 59) {
+        }else if (marks > 49 && marks <= 59) { 
             return "C";
         }else if (marks > 40 && marks <=49) {
             return "D";
